bd9d99519a3627fdffa50621d1437aa572897dfb
Ed Carter <bittenbytailfly@server.fake>
2020-01-19 23:16:25 +0000

    DIfferentiation between errors and timeouts

--- lib/screens/ListingsScreen.dart
+++ lib/screens/ListingsScreen.dart
@@ -47,7 +47,9 @@ class _ListingPageState extends State<ListingsScreen> {
       this._listingTypeFilter.addListener(_updateListings);
 
       this._updateListings();
-    }).timeout(const Duration(seconds: 10), onTimeout: _filterListingsError);
+    })
+    .timeout(const Duration(seconds: 10), onTimeout: _filterListingsError)
+    .catchError((error) => _filterListingsError());
   }
 
   Future _refreshListings() async {
@@ -56,7 +58,9 @@ class _ListingPageState extends State<ListingsScreen> {
         listingsService.getListings().then((listings) {
           this._listings = listings.cast<Listing>();
           _updateListings();
-        }).timeout(const Duration(seconds: 10), onTimeout: _filterListingsError);
+        })
+        .timeout(const Duration(seconds: 10), onTimeout: _filterListingsError)
+        .catchError((error) => _filterListingsError());
       });
     });
   }
@@ -112,7 +116,7 @@ class _ListingPageState extends State<ListingsScreen> {
                 _buildGenreFilterSliver(largerFontStyle),
               ]);
             } else if (snapshot.hasError) {
-              throw new Exception();
+              //throw new Exception();
             }
             return _buildLoadingIndicator();
           }),
